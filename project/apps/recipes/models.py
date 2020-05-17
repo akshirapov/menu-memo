0218a49b2113eb6c7c924aa950d90771aee29d2e
Alexander Shirapov <cccp2006_06@mail.ru>
2020-05-18 04:04:45 +0800

    Add models to recipes app.

---
+++ project/apps/recipes/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=255,
+        db_index=True, unique=True
+    )
+    image = ProcessedImageField(
+        upload_to='ingredients',
+        default='ingredients/ingredient_default.jpg',
+        processors=[ResizeToFill(1024, 768)],
+        format='JPEG',
+        options=[{'quality': 60}]
+    )
+    description = models.TextField(
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'ingredients'
+
+
+class Dish(models.Model):
+    name = models.CharField(
+        max_length=255,
+        db_index=True
+    )
+
+    image = ProcessedImageField(
+        upload_to='dishes',
+        default='dishes/dish_default.jpg',
+        processors=[ResizeToFill(1024, 768)],
+        format='JPEG',
+        options=[{'quality': 60}]
+    )
+    description = models.TextField(
+        blank=True
+    )
+    recipe = models.TextField(
+        blank=True
+    )
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'dishes'
--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""recipes URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('recipes/', include('recipes.apps.recipes.urls')),
+    path('recipes/', include('project.apps.recipes.urls')),
     path('admin/', admin.site.urls),
 ]
