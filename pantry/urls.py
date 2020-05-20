ced8eacc9a359c8276678f1f65fb519bb5eafc6b
Alexander Shirapov <cccp2006_06@mail.ru>
2020-05-21 03:00:52 +0800

    Renaming project.

--- pantry/urls.py
+++ pantry/urls.py
@@ -1,4 +1,4 @@
-"""project URL Configuration
+"""pantry URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -21,8 +21,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('project.apps.recipes.urls')),
+    path('', include('pantry.apps.recipes.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns  += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
